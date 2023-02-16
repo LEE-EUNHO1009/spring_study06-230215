c6301056593ad70e7a44c726653494e8238f52c4
ici <ici@DESKTOP-G48V45S>
2023-02-16 11:57:54 +0900

    write 기능 추가

--- src/main/java/com/eunocompany/home4/command/BWriteCommand.java
+++ src/main/java/com/eunocompany/home4/command/BWriteCommand.java
@@ -1,13 +1,29 @@
 package com.eunocompany.home4.command;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.ui.Model;
+
+import com.eunocompany.home4.dao.BDao;
 
 public class BWriteCommand implements BCommand {
 
 	@Override
 	public void execute(Model model) {
 		// TODO Auto-generated method stub
-
+		Map<String, Object> map = model.asMap();
+		//model 객체 안에 있는 request를 map으로 매핑
+		HttpServletRequest request = (HttpServletRequest) map.get("request");
+		
+		String bname = request.getParameter("bname");
+		String btitle = request.getParameter("btitle");
+		String bcontent = request.getParameter("bcontent");
+		
+		BDao dao=new BDao();
+		dao.write(bname, btitle, bcontent);
+		
 	}
 
 }
--- src/main/java/com/eunocompany/home4/controller/BController.java
+++ src/main/java/com/eunocompany/home4/controller/BController.java
@@ -1,11 +1,15 @@
 package com.eunocompany.home4.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.eunocompany.home4.command.BCommand;
 import com.eunocompany.home4.command.BListCommand;
+import com.eunocompany.home4.command.BWriteCommand;
 
 @Controller
 public class BController {
@@ -27,5 +31,15 @@ public class BController {
 	@RequestMapping(value="writeForm")
 	public String writeForm() {
 		return "write_form";
+	}
+	@RequestMapping(value="write", method=RequestMethod.POST)
+	public String write(HttpServletRequest request, Model model) {
+		
+		model.addAttribute("request", request);  //리퀘스트 객체를 모델에 실어서 보내준다.
+		
+		command = new BWriteCommand();
+		command.execute(model);
+		
+		return "redirect:list";
 	}
 }
